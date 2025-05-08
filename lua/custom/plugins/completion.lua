80c0b0cf2fedfce2b0811812f8b9acc3dd37b72e
yanmo <jrsky723@gmail.com>
2025-05-08 15:34:36 +0900

    add harpoon file name

--- lua/custom/plugins/completion.lua
+++ lua/custom/plugins/completion.lua
@@ -53,6 +53,18 @@ return {
 			-- 	default = { "lsp", "path", "snippets", "buffer" },
 			-- },
 		},
+		sources = {
+			default = { "lsp", "path", "snippets", "buffer" },
+			providers = {
+				path = {
+					opts = {
+						get_cwd = function(_)
+							return vim.fn.getcwd()
+						end,
+					},
+				},
+			},
+		},
 		-- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
 		-- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
 		-- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -1,12 +1,14 @@
 return {
 	"ThePrimeagen/harpoon",
 	branch = "harpoon2",
+	dependencies = { "nvim-lua/plenary.nvim", "folke/which-key.nvim" }, -- No nvim-web-devicons needed
 	config = function()
-		---
 		local harpoon = require("harpoon")
+		local wk = require("which-key")
+
 		harpoon:setup()
 
-		-- Change these to leader-based mappings instead of Alt/Meta
+		-- Define static mappings with vim.keymap.set
 		vim.keymap.set("n", "<leader>ha", function()
 			harpoon:list():add()
 		end, { desc = "[H]arpoon [A]dd file" })
@@ -15,11 +17,25 @@ return {
 			harpoon.ui:toggle_quick_menu(harpoon:list())
 		end, { desc = "[H]arpoon [L]ist" })
 
-		-- Keep the number shortcuts but add descriptions
+		-- Add dynamic mappings for <leader>1 to <leader>5
 		for _, idx in ipairs({ 1, 2, 3, 4, 5 }) do
-			vim.keymap.set("n", string.format("<leader>%d", idx), function()
-				harpoon:list():select(idx)
-			end, { desc = string.format("Harpoon file %d", idx) })
+			wk.add({
+				{
+					string.format("<leader>%d", idx),
+					function()
+						harpoon:list():select(idx)
+					end,
+					desc = function()
+						local items = harpoon:list().items
+						return items[idx] and vim.fn.fnamemodify(items[idx].value, ":t") or ""
+					end,
+					mode = "n",
+					icon = {
+						icon = "󰈮",
+						color = "azure",
+					},
+				},
+			})
 		end
 	end,
 }
