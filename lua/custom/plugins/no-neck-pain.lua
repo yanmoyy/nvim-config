67b274e2d680fe329a1eb4a6df25fe5e8d972cbc
yanmo <67538999+yanmoyy@users.noreply.github.com>
2025-10-27 09:01:15 +0900

    add im-select

--- lua/custom/plugins/no-neck-pain.lua
+++ lua/custom/plugins/no-neck-pain.lua
@@ -3,5 +3,8 @@ return {
 	version = "*",
 	config = function()
 		vim.keymap.set("n", "<leader>z", "<cmd>NoNeckPain<cr>", { noremap = true, desc = "[Z]en mode" })
+		require("no-neck-pain").setup({
+			width = 120,
+		})
 	end,
 }
