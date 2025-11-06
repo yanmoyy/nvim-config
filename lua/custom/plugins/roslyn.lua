2a8edacc5cd2d62a2ae0775a6a10b433a3ed7b54
yanmo <67538999+yanmoyy@users.noreply.github.com>
2025-11-06 11:39:18 +0900

    roslyn update

--- lua/custom/plugins/roslyn.lua
+++ lua/custom/plugins/roslyn.lua
@@ -5,21 +5,4 @@ return {
 	opts = {
 		-- your configuration comes here; leave empty for default settings
 	},
-
-	config = function()
-		vim.lsp.config("roslyn", {
-			on_attach = function()
-				print("This will run when the server attaches!")
-			end,
-			settings = {
-				["csharp|inlay_hints"] = {
-					csharp_enable_inlay_hints_for_implicit_object_creation = true,
-					csharp_enable_inlay_hints_for_implicit_variable_types = true,
-				},
-				["csharp|code_lens"] = {
-					dotnet_enable_references_code_lens = true,
-				},
-			},
-		})
-	end,
 }
