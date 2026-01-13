2adc9e0be4105501276c556d805988e837041cd5
yanmo <67538999+yanmoyy@users.noreply.github.com>
2026-01-13 09:42:11 +0900

    update for im-select

--- lua/custom/plugins/im-select.lua
+++ lua/custom/plugins/im-select.lua
@@ -20,7 +20,7 @@ return {
 			-- For Windows/WSL, default: "im-select.exe"
 			-- For macOS, default: "macism"
 			-- For Linux, default: "fcitx5-remote" or "fcitx-remote" or "ibus"
-			default_command = "im-select",
+			default_command = is_mac and "im-select" or "im-select.exe",
 
 			-- Restore the default input method state when the following events are triggered
 			-- "VimEnter" and "FocusGained" were removed for causing problems, add it by your needs
